40a8ba15043bc86255b84af858d63c078247c8f6
CaneTLOTW <35076818+CaneTLOTW@users.noreply.github.com>
2026-08-13 17:40:28 +0200

    chore: bump dashboard frontend cache version

--- custom_components/e_c3_dashboard/const.py
+++ custom_components/e_c3_dashboard/const.py
@@ -28,7 +28,7 @@ DEFAULT_OPTIONS = {
 }
 
 FRONTEND_URL = "/e_c3_dashboard/e_c3_dashboard.js"
-FRONTEND_VERSION = "0.4.0"
+FRONTEND_VERSION = "0.4.1"
 STATIC_VERSION = FRONTEND_VERSION
 FRONTEND_RESOURCE_URLS = (
     "/e_c3_dashboard/trip-history-card.js",
